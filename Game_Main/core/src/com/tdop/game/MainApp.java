7f308dba97df0d7226b649cd9277ab1b207d2f58
Samuel Levi <samuel.levicsdev@gmail.com>
2023-10-31 17:24:22 -0300

    Estruturacao dos arquivos completa

--- Game_Main/core/src/com/tdop/game/MainApp.java
+++ Game_Main/core/src/com/tdop/game/MainApp.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
@@ -19,38 +21,59 @@ import com.badlogic.gdx.utils.ScreenUtils;
 */
 
 public class MainApp extends ApplicationAdapter {
-	Stage stage;
-	TextField texto;
+	private SpriteBatch batch;
+	private Texture fundo;
+	private Rectangle fundo_ret;
+	//private Stage stage;
+	private TextField texto;
+	private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
-		stage = new Stage();
-		Gdx.input.setInputProcessor(stage);
+		//stage = new Stage();
+		//Gdx.input.setInputProcessor(stage);
 
+		batch =  new SpriteBatch();
+		fundo = new Texture("tela_de_fundo.png");
+		fundo_ret = new Rectangle();
+		fundo_ret.height = 720;
+		fundo_ret.width = 1080;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1080, 720);
+
+		/*
 		Skin skin = new Skin();
 		TextFieldStyle style = new TextFieldStyle();
 		style.fontColor = Color.WHITE; //Define a cor do texto.
 		skin.add("default", style);
-
 		texto = new TextField("Não há nada aqui ainda...", skin);
-		texto.setPosition(640, 360); //Posição do texto dentro codigo.
 		texto.setSize(300, 80);
-		stage.addActor(texto);
+		*/
+		//stage.addActor(texto);
 
 
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.setProjectionMatrix(camera.combined);
+		batch.draw(fundo, 0, 0);
+		batch.end();
+		
+		/* 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
+		*/
 	}
 	
 	@Override
 	public void dispose () {
-		stage.dispose();
+		batch.dispose();
+		fundo.dispose();
+		//stage.dispose();
 	}
 }
--- Game_Main/desktop/src/com/tdop/game/DesktopLauncher.java
+++ Game_Main/desktop/src/com/tdop/game/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("TheDepthsOfPrekirs");
-		config.setWindowedMode(1280, 720);
+		config.setWindowedMode(1080, 720);
 		config.useVsync(true);
 		new Lwjgl3Application(new MainApp(), config);
 	}
